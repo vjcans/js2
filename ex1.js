edec002d3fe9833ce6d532a33c9f550d86193cc5
vjcans <19268324+vjcans@users.noreply.github.com>
2023-04-11 14:43:44 +1000

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -7,13 +7,13 @@
 // Current Count : 4
 // Loop stopped!
 // The following code has 4 errors, find them and fix them and use node to check the output.
-const count = 0;
+let count = 0;
             
-            console("Starting Loop");
+            console.log("Starting Loop");
             do {
                console.log("Current Count : " + count);
-               count--;
+               count++;
             }
             
-            then (count < 5);
-            console.log ("Loop stopped!);
+            while (count < 5);
+            console.log ("Loop stopped!");
